673af570a59fb1d16b69015cdb687ed883449dd6
admin <admin@YD03-18>
2020-06-05 16:25:37 +0900

    commit

---
+++ JavaFXNewProject/src/teamProject/OrderMainControll.java
@@ -0,0 +1,15 @@
+package teamProject;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.Initializable;
+
+public class OrderMainControll implements Initializable{
+
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		
+	}
+
+}
