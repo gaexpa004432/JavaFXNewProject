084dabe5ee93d67bdc71cd0c664133329fc1ac45
admin <admin@YD03-08>
2020-06-09 10:26:22 +0900

    200609 10:26

--- JavaFXNewProject/src/teamProject/ProjectMain.java
+++ JavaFXNewProject/src/teamProject/ProjectMain.java
@@ -11,11 +11,13 @@ public class ProjectMain extends Application{
 
 	@Override
 	public void start(Stage arg0) throws Exception {
-		Parent root = FXMLLoader.load(getClass().getResource("OrderMain.fxml"));
+		Parent root = FXMLLoader.load(getClass().getResource("login.fxml"));
 		Scene scene = new Scene(root);
 		arg0.setScene(scene);
 		arg0.setTitle("배달앱");
 		arg0.show();
+		scene.getStylesheets().add(getClass().getResource("login.css").toString());
+		
 	}
 	
 	public static void main(String[] args) {
