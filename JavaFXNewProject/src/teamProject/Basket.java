98d82d53a60b9aed1ec24f3e781d5514e7e26023
김린아 <missingyoujj@knou.ac.kr>
2020-06-15 22:15:09 +0900

    200615

--- JavaFXNewProject/src/teamProject/Basket.java
+++ JavaFXNewProject/src/teamProject/Basket.java
@@ -3,31 +3,32 @@ package teamProject;
 import javafx.beans.property.SimpleIntegerProperty;
 import javafx.beans.property.SimpleStringProperty;
 
-public class Basket {
-	private String myid;
-	private int myrest;
-	private int myfood;
-	public int getMyrest() {
-		return myrest;
+public class Basket {  //바스켓클래스선언
+	private String myid; //같은클래스 내부에만 접근가능한 문자타입 myid 변수 선언
+	private int myrest; //같은클래스 내부에만 접근가능한 int타입 myrest 변수 선언
+	private int myfood; //같은클래스 내부에만 접근가능한 int타입 myfood 변수 선언
+	
+	public int getMyrest() {  //getMyrest() 생성자 생성
+		return myrest;  // return값은 myrest
 	}
 
-	public void setMyrest(int myrest) {
-		this.myrest = myrest;
+	public void setMyrest(int myrest) {  //setMyrest(int myrest) 생성자생성
+		this.myrest = myrest;  // myrest변수를 myrest전역변수에 넣음
 	}
 
-	public int getMyfood() {
-		return myfood;
+	public int getMyfood() {  //getMyfood() 생성자생성
+		return myfood;  // return값은 myfood
 	}
 
-	public void setMyfood(int myfood) {
-		this.myfood = myfood;
+	public void setMyfood(int myfood) {  //setMyfood(int myfood) 생성자생성
+		this.myfood = myfood;  // myfood변수를 myfood전역변수에 넣음
 	}
 
-	public String getMyid() {
-		return myid;
+	public String getMyid() {  //getMyid() 생성자생성
+		return myid;  // return값은 myid
 	}
 
-	private SimpleStringProperty custId;
+	private SimpleStringProperty custId; //같은클래스 내부에만 접근가능한 문자타입 myid 변수 선언
 	private SimpleStringProperty restId;
 	private SimpleIntegerProperty foodId;
 	
--- JavaFXNewProject/src/teamProject/ProjectMain.java
+++ JavaFXNewProject/src/teamProject/ProjectMain.java
@@ -11,11 +11,11 @@ public class ProjectMain extends Application{
 
 	@Override
 	public void start(Stage arg0) throws Exception {
-		Parent root = FXMLLoader.load(getClass().getResource("login.fxml"));
-		Scene scene = new Scene(root);
-		arg0.setScene(scene);
-		arg0.setTitle(" Delivery APP ");
-		arg0.show();
+		Parent root = FXMLLoader.load(getClass().getResource("login.fxml")); //login.fxml 불러와서 root에 담기
+		Scene scene = new Scene(root); //root를 scene 인스턴스에 담기
+		arg0.setScene(scene);  //arg0의 scene을 설정
+		arg0.setTitle(" Delivery APP ");  //arg0의 타이틀이름 설정
+		arg0.show();  //arg0을 보여주게하기
 		
 	}
 	
