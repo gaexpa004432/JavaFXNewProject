57125c0a0393186dd8b949d6148fee978c4e6854
admin <admin@YD03-08>
2020-06-16 09:53:28 +0900

    200616

--- JavaFXNewProject/src/teamProject/OrderMainControll.java
+++ JavaFXNewProject/src/teamProject/OrderMainControll.java
@@ -55,17 +55,18 @@ public class OrderMainControll implements Initializable {
 	@FXML
 	public void login(MouseEvent event) {
 
-//			Customer cus = new Customer(id.getText(),password.getText());
-//			boolean login = db.dbselect(cus);
-//			if(login == true) {
-//			Parent node = (Parent) event.getSource();
-//			Stage stage = (Stage) node.getScene().getWindow();
-//				stage.close();
+			Customer cus = new Customer(id.getText(),password.getText());
+			boolean login = db.dbselect(cus);
+			if(login == true) {
+				this.myid = id.getText();
+			Parent node = (Parent) event.getSource();
+			Stage stage = (Stage) node.getScene().getWindow();
+				stage.close();
 
 		
 				ordermain();
 				
-//		}
+		}
 	
 	}
 
